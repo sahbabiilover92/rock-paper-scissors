94cee7d0ee69495d9a4ff9a7d6e05f741c777ab7
sahbabiilover92 <257049988+sahbabiilover92@users.noreply.github.com>
2026-02-25 19:15:21 -0500

    Create playRound function and allow it take take the human input and convert it to lowercase

--- script.js
+++ script.js
@@ -21,12 +21,11 @@ console.log(getComputerChoice())
 //Function that takes human input of rock paper or scissors. Stores input
 function getHumanChoice() {
    //input prompt
-    let userInput = prompt("Rock, paper, or scissors?", "");
+    let userInput = prompt("Rock, paper, or scissors?");
     return userInput
     
 }
 
-console.log(getHumanChoice())
 
 //Create variables that store human and computer scores
 
@@ -34,3 +33,19 @@ humanScore=0;
 computerScore=0;
 
 console.log(humanScore,computerScore);
+
+/* Function that takes two arguments, one from the user and one from the computer. 
+It should convert the user string into one of the 3 strings stored in the computer responses (ie Rock becomes rock)
+Function compares the two values presented by computer and user so that rock paper scissors logic is followed (ie rock beats scissors, paper beats rock, etc.)
+After comparison, the console returns a statement with that rounds winner
+Each round win contributes to the winners score (ie if computer wins computer score goes up 1)*/
+
+userChoice = getHumanChoice();
+
+function playRound(humanChoice, computerChoice){
+    let humanCase= humanChoice.toLowerCase();
+    return humanCase
+
+}
+
+console.log(playRound(userChoice))
